1d03a02d7aad2c45c4ed0a53ca8701711236887d
Helper Yoo <helper@purding.com>
2021-06-06 01:55:25 +0900

    Update code

--- yellow_music_player/script.js
+++ yellow_music_player/script.js
@@ -9,34 +9,40 @@ const playBtn = document.getElementById('play');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 
+const artistName = document.getElementById('artist-name');
 const songTitle = document.getElementById('song-title');
 const songPlayTime = document.getElementById('song-play-time');
 
 // Music album
-const album = [
-  'I Will Always Love You',
-  'I Have Nothing',
-  'Im Every Woman',
-  'Run To You',
-  'Queen Of The Night',
-];
+const album = {
+  artist: 'Whitney Houston',
+  title: 'The Bodyguard',
+  songs: [
+    'I Will Always Love You',
+    'I Have Nothing',
+    'Im Every Woman',
+    'Run To You',
+    'Queen Of The Night',
+  ],
+};
+
+console.log(album['artist']);
 
 // Song Index of album
 let songIndex = 0;
 
 // Initial load song
-loadSong(album[songIndex]);
+let artist = album['artist'];
+let title = album['title'];
+let songs = album['songs'];
+
+loadSong(artist, title, songs[songIndex]);
 
 // Update song info
-function loadSong(album) {
-  songTitle.innerText = album;
-  audio.src = `music/${album}.mp3`;
-}
-
-function playerList(album) {
-  album.forEach((song) => {
-    musicPlayerList.innerHTML += `<li class="player-list-item">${song}</li>`;
-  });
+function loadSong(artist, album, songs) {
+  artistName.innerText = artist;
+  songTitle.innerText = `${album} · ${songs}`;
+  audio.src = `music/${songs}.mp3`;
 }
 
 // Play song
@@ -59,11 +65,11 @@ function pauseSong() {
 function nextSong() {
   songIndex++;
 
-  if (songIndex > album.length - 1) {
+  if (songIndex > songs.length - 1) {
     songIndex = 0;
   }
 
-  loadSong(album[songIndex]);
+  loadSong(artist, title, songs[songIndex]);
   playSong();
 }
 
@@ -72,10 +78,10 @@ function prevSong() {
   songIndex--;
 
   if (songIndex < 0) {
-    songIndex = album.length - 1;
+    songIndex = album['songs'].length - 1;
   }
 
-  loadSong(album[songIndex]);
+  loadSong(artist, title, songs[songIndex]);
   playSong();
 }
 
@@ -109,5 +115,3 @@ audio.addEventListener('timeupdate', updateProgress);
 audio.addEventListener('ended', nextSong);
 
 progressBar.addEventListener('click', setProgress);
-
-window.onload = playerList(album);
